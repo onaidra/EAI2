26b3d96c468cafa007d0613781a126d39e3ffdba
onaidra <adriano.puglisi.193@gmail.com>
2022-10-22 17:46:33 +0200

    main

--- EAI2Project/main.py
+++ EAI2Project/main.py
@@ -1,36 +1,39 @@
 from modim_classes import CleanScreen
 import sys
-from global_executor import run_executor
-from scripts import Sonar, Dialogue,Touch
+from scripts import Sonar, Dialogue,Gestures
 import math
 import pepper_cmd
 sys.path.append('tablet/scripts')
 
+dialogue = Dialogue()
 
+def main_run():
+    interaction = dialogue.start_interaction()
+    if (interaction):
+        dialogue.say("Do you need indications or do you want to play?")
+        answer = dialogue.listen()
+        if answer == "Indications":
+            dialogue.say("Perfect! Where do you have to go?")
+            answer = dialogue.listen() #AGGIUNGERE VOCABOLARIO CON TUTTE LE DESTINAZIONI POSSIBILI 
+            dialogue.say("Ok, well now using the tablet press on the cells in which are obstacles, if None press the Ok button")
+            #APRI INTERAZIONE CON MAPPA SUL TABLET 
+        else:
+            dialogue.say("Perfect! Let's play a memory game on the tablet")
+            #APRI INTERAZIONE CON GIOCO SU TABLET
+    return
+    
 if __name__ == "__main__":
-	"""
+
 	print("Starting monitor, to stop robot send KeyboardInterrupt signal.")
 	while True:
 		try:
 			sonar = Sonar()
-			location = sonar.listen(personHere=False)
-			if location == 'front':
-				executor_status = run_executor()
+			check = sonar.listen(personHere=False)
+			if check == 'front':
+				executor_status = main_run()
 				if executor_status == -1:
 					print("Interaction with user completed successfully.")
-			else:
-				dialogue = Dialogue()
-				dialogue.say(
-					"Who is behind me? If you came to me, come please in front so I could see you.")
-				print("Person was detected by rear sonar.")
 		except KeyboardInterrupt:
 			print("Interrupted")
 			CleanScreen()
 			sys.exit(0)
-	"""
-	touch = Touch()
-	a=math.pi/180
-	touch.change_pose([7, 10, 11],
-							[a*50, a*60, -a*90],
-							pepper_cmd.robot.getPosture(),
-							0.1)
--- EAI2Project/scripts.py
+++ EAI2Project/scripts.py
@@ -8,6 +8,10 @@ import wave
 import contextlib
 import math
 from numpy.random import choice
+def start_interaction():
+    Dialogue.say("Hi,I'm pepper, touch my head to interact with me!.")
+    return Gestures.head_interaction(wait_ = 10.0) # waiting for python touch_sim.py --sensor HeadMiddle
+
 begin()
 class InitRobot():
 
@@ -17,7 +21,7 @@ class InitRobot():
   
 class Gestures:
 	def __init__(self, touched=False):
-		InitRobot(speed=200)
+		InitRobot(speed=100)
 		# joint angles should be given in radians
 		# 0 -  HeadYaw,        1 -  HeadPitch
 		# 2 -  LShoulderPitch, 3 -  LShoulderRoll, 4 -  LElbowYaw, 5 -  LElbowRoll, 6 -  LWristYaw
@@ -29,25 +33,21 @@ class Gestures:
                      "LHand", "RHand", "HipRoll", "HipPitch", "KneePitch"]
 		self.touched = touched
 
-	def change_pose(self, indices, values, pose, sleeping_time=0.5):
-
-		joint_list = []
+	def change_pose(self, indices, values, pose):
+     
 		for idx, val in zip(indices, values):
 			pose[idx] = val
-			joint_list.append(self.jointNames[idx])
-
 		pepper_cmd.robot.setPosture(pose)
 		
-
 		return pose
 
-	def monitor_touch(self, monitoring_time=20.0):
+	def head_interaction(self, wait_=20.0):
 
 		curr_time = time.time()  # start timer
 
 		pepper_cmd.robot.startSensorMonitor()
-		print("Waiting a touch to start during {} seconds...".format(monitoring_time))
-		while not self.touched and (time.time() - curr_time < monitoring_time):
+		print("Waiting a touch to start during {} seconds...".format(wait_))
+		while not self.touched and (time.time() - curr_time < wait_):
 			p = pepper_cmd.robot.sensorvalue()
 			self.touched = (p[3] > 0)  # head touch sensor
 		pepper_cmd.robot.stopSensorMonitor()
@@ -64,16 +64,30 @@ class Gestures:
 	def sayhi(self):
 		a=math.pi/180
   
-		self.change_pose([7,8,10], #7 -> -20 ; 10 -> 90
+		self.change_pose([7,8,10], 
 							[-20*a,-90*a,120*a],
-							pepper_cmd.robot.getPosture(),
-							0.5)
-		self.change_pose([7,8,10], #7 -> -20 ; 10 -> 90
+							pepper_cmd.robot.getPosture())
+		self.change_pose([7,8,10],
 							[-20*a,-90*a,60*a],
-							pepper_cmd.robot.getPosture(),
-							0.5)
+							pepper_cmd.robot.getPosture())
 
-		pepper_cmd.robot.green_eyes()
 		return pepper_cmd.robot.getPosture()
+class Dialogue:
 
+	def __init__(self, speed=100):
+		InitRobot(speed=speed)
+
+	def say(self, sentence, require_answer=False, sleeping_time=0.0):
+		pepper_cmd.robot.say(sentence)
+		if require_answer:
+			return self.listen(timeout=30)
+		if sleeping_time:
+			time.sleep(sleeping_time)
+
+	def listen(self, vocabulary=["Play, Indication"], timeout=30):
+		answer = pepper_cmd.robot.asr(vocabulary=vocabulary, timeout=timeout)
+		while not answer:
+			answer = self.say(
+				sentence="Sorry, I did not hear you, repeat please.", require_answer=True)
+		return answer
 end()
